71c68fd7f74a03f5d8b9fce434307e5787a47033
SwathikaGG <85396265+SwathikaGG@users.noreply.github.com>
2021-06-05 17:25:00 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Purpose of World Environment Day</h1>
+      <p>
+      World Environment Day is observed on June 5 every year. The purpose of World Environment Day is to spread awareness about the threat to the environment due to rising pollution levels and climate change.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
